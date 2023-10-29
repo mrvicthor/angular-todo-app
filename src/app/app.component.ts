3c010915f59ad91f862f90637de2e5254cc22d6e
Victor Eleanya <victoreleanya89@gmail.com>
2023-10-29 18:51:18 +0000

    my first angular directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/shared/model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app';
+
+  todos: Todo[] = [
+    { id: 1, title: 'Learn Angular', completed: false },
+    { id: 2, title: 'Get better', completed: false },
+    { id: 3, title: 'Continue learning', completed: false },
+  ];
 }
