cdd03157545889c903ea10522b96972496434c43
Victor Eleanya <victoreleanya89@gmail.com>
2023-10-31 21:18:15 +0000

    added assets

---
+++ src/app/components/form-wrapper/form-wrapper.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-wrapper',
+  templateUrl: './form-wrapper.component.html',
+  styleUrls: ['./form-wrapper.component.css'],
+})
+export class FormWrapperComponent {
+  isDark: boolean = false;
+  isDarkBg: string = '../../../assets/images/icon-sun.svg';
+  isLightBg: string = '../../../assets/images/icon-moon.svg';
+  toggleBackgroundImage() {
+    this.isDark = !this.isDark;
+    console.log(this.isDark);
+  }
+}
